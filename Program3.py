66256c82dbf3651e99da0bf4cdad4937fdd0b450
Eaindra Naing <79598728+eaindra-naing@users.noreply.github.com>
2021-11-26 03:48:24 +0000

    got part 3 down

--- Program3.py
+++ Program3.py
@@ -4,11 +4,14 @@ PROGRAM 3
 *********
 Define a function second_smallest that takes a list of integers or floats as an argument. The function returns the 2nd smallest number in the list.
 '''
-def second_smallest(lst):
-  pass #delete this line when you start writing your code
+def second_smallest(lis):
+  lis.sort() 
+  return (lis[1])
 
 
 
-
-
-#need to work on
+"""
+elif program == '3':
+  lis = [int(i) for i in input("Enter a list of numbers separated by a space: ").split()]
+  print(second_smallest(lis))
+"""
